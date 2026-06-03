08d53462120c03f2a011a1cadd370ec5d7be4017
Kevin Ramos <47020463+eosin-kramos@users.noreply.github.com>
2026-06-03 12:46:29 -0700

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,24 @@
+#Questions: Is this already ordered? 
+# iterate through array, and add it to map. Add to value by 1
+# we create a bucket, with length being len(nums), 
+# iterate through map, we use value as the index, and in that index, we add key
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        bucket = [[] for _ in range(len(nums) + 1)]
+        frequency = {}
+
+        for num in nums:
+            frequency[num] = frequency.get(num, 0) + 1
+        
+        for key, value in frequency.items():
+            bucket[value].append(key)
+
+        result = []
+
+        for i in range(len(bucket) - 1, -1, -1):
+            for num in bucket[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
+
+
